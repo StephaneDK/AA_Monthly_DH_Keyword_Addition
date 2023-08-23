5976bbd5fb471842d70637d0b84b50f93c395f44
Stephane <stephane.nichanian@gmail.com>
2023-08-23 10:45:39 +0100

    Shortened pipeline run

--- Pipeline.sh
+++ Pipeline.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+source "C:\Users\steph\miniconda3\etc\profile.d\conda.sh"
+#source "C:\Users\snichanian\miniconda3\etc\profile.d\conda.sh"
+conda activate kadd_env
+
 #Keyword Addition path
 path_local='C:\Users\steph\Documents\DK\Work\Forecasting book sales and inventory\Auxilary tasks\Amazon Advertisment\Keyword Addition\Monthly\Code\'
 path_DK='C:\Users\snichanian\Documents\DK\Work\Forecasting book sales and inventory\Auxilary tasks\Amazon Advertisment\Keyword Addition\Monthly\Code\'
@@ -16,7 +20,7 @@ python "fetch_data_snfk_dly.py"
 
 Rscript "New Keywords.R"
 
-
+conda deactivate
 
 
 
